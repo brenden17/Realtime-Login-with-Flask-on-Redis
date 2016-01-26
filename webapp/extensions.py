f8239a8d84c1d7b90b5e675aa488bd049567c332
brenden17 <chulwook.jeon@gmail.com>
2016-01-26 22:05:26 +1000

    use pipeline

--- webapp/extensions.py
+++ webapp/extensions.py
@@ -26,22 +26,23 @@ class LoginStatusSession(object):
         if not user.is_anonymous:
             # remove all users who logined ago KEEPING_TIME sec.
             timestamp = time()
+            pipe = redis.pipeline()
             redis.zremrangebyscore('loginstatus', 0, timestamp-KEEPING_TIME)
             # added user
             redis.zadd('loginstatus', user.username, timestamp)
-
+            pipe.execute()
             user_friends = user.get_friends() or []
             if user_friends:
                 friends_key = 'friend:{}'.format(user.username)
-                redis.sadd(friends_key, *user_friends)
+                pipe.sadd(friends_key, *user_friends)
                 # save friends for KEEPING_TIME sec
-                redis.expire(friends_key, KEEPING_TIME)
+                pipe.expire(friends_key, KEEPING_TIME)
             
                 # get frineds who logined             
                 logined_friend_key = 'lfriend:{}'.format(user.username)
-                redis.zinterstore(logined_friend_key, [friends_key, 'loginstatus'])
-                logined_friends = redis.zrange(logined_friend_key, 0,-1, withscores=False)
-
+                pipe.zinterstore(logined_friend_key, [friends_key, 'loginstatus'])
+                pipe.zrange(logined_friend_key, 0,-1, withscores=False)
+                logined_friends = pipe.execute()[-1]
         return dict(user=user,
                     user_friends=user_friends,
                     logined_friends=logined_friends)
